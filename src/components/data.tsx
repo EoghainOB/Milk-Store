d816b97f1e157d9e3a45b2358b37dcd3ef9b17e0
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-01-29 14:59:16 +0100

    merge readme

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -11,7 +11,7 @@ const Data = ({ children }: any) => {
 
     useEffect(() => {
       axios
-      .get('/api/milk')
+      .get('http://localhost:8080/api/milk')
       .then((response) => {
       setProducts([...response.data])
       })
