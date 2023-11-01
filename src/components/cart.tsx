3a063749870f7003ad9e4a94e89da7558ff361c1
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-11-01 23:08:02 +0100

    updates

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,60 +1,64 @@
-import React, { useContext } from 'react'
-import { ProductContextType } from '../types';
-import Cartdetails from './cartdetails';
-import { ProductContext } from './data';
-import Navigation from './navigation';
+import React, { useContext } from "react";
+import { ProductContextType } from "../types";
+import Cartdetails from "./cartdetails";
+import { ProductContext } from "./data";
+import Navigation from "./navigation";
 
 const Cart = () => {
-   
-  const { cart } = useContext(ProductContext) as ProductContextType
+  const { cart } = useContext(ProductContext) as ProductContextType;
 
   const purchaseMilk = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log("Will implement this later")
-    const data = cart
-    await fetch('http://localhost:8080/cart', {
-      method: 'POST',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
+    console.log("Will implement this later");
+    const data = cart;
+    await fetch("http://localhost:8080/cart", {
+      method: "POST",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
     });
   };
 
   return (
     <>
-    <div className="navigate">
-    <Navigation />
-    <div className='cart'>
-      <h2 className='cartName'>My Milk Cart</h2>
-        {cart.length === 0 && <div className='cartEmpty'>
-          <h1>Your Milk cart is Empty</h1>
-        </div>}
-      {cart.length !== 0 && <><div className='cartHeaders'>
-        <div className='cartProduct'>
-          <h3>Product</h3>
-        </div>
-        <div className='cartQty'>
-          <h3>Qty</h3>
-        </div>
-        <div className='cartRemove'>
-          <h3>Remove?</h3>
+      <div className="navigate">
+        <Navigation />
+        <div className="cart">
+          <h2 className="cartName">My Milk Cart</h2>
+          {cart.length === 0 && (
+            <div className="cartEmpty">
+              <h1>Your Milk cart is Empty</h1>
+            </div>
+          )}
+          {cart.length !== 0 && (
+            <>
+              <div className="cartHeaders">
+                <div className="cartProduct">
+                  <h3>Product</h3>
+                </div>
+                <div className="cartQty">
+                  <h3>Qty</h3>
+                </div>
+                <div className="cartRemove"></div>
+              </div>
+              <hr />
+              {cart.map((data, index) => (
+                <div key={index}>
+                  <Cartdetails cartitem={data} />
+                </div>
+              ))}
+              <hr />
+              <div className="purchaseMilk">
+                <button type="button" onClick={purchaseMilk}>
+                  Complete Order
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
-        <hr/>
-        {cart.map((data, index) => (
-        <div key={index}>
-          <Cartdetails cartitem={data}/>
-        </div>
-        ))}
-        <hr/>
-        <div className='purchaseMilk'>
-          <button type="button" onClick={purchaseMilk}>Complete Order</button>
-        </div>
-        </>}
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default Cart;
--- src/components/cartdetails.tsx
+++ src/components/cartdetails.tsx
@@ -1,44 +1,46 @@
-import React, { useContext } from 'react'
-import { cartTypes, ProductContextType } from '../types';
-import { ProductContext } from './data';
+import React, { useContext } from "react";
+import { cartTypes, ProductContextType } from "../types";
+import { ProductContext } from "./data";
 
 export type Props = {
   cartitem: cartTypes;
-}
+};
 
-const Cartdetails = ({cartitem}: Props) => {
+const Cartdetails = ({ cartitem }: Props) => {
+  const { setCart } = useContext(ProductContext) as ProductContextType;
 
-  const { setCart } = useContext(ProductContext) as ProductContextType
-  
   const deleteItem = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const items = JSON.parse(localStorage.getItem('cart') || '{}')
-      for (let i = 0; i < items.length; i++) {
-        if (items[i]._id === cartitem._id) {
-          items.splice(i, 1);
-        }
-        localStorage.setItem('cart', JSON.stringify(items));
-        setCart(items)
+    const items = JSON.parse(localStorage.getItem("cart") || "{}");
+    for (let i = 0; i < items.length; i++) {
+      if (items[i]._id === cartitem._id) {
+        items.splice(i, 1);
       }
-  }
-  
+      localStorage.setItem("cart", JSON.stringify(items));
+      setCart(items);
+    }
+  };
+
   return (
     <>
-    <div className='cartContents'>
-      <div className='cartData'>
-        <div className='cartProduct'>
-          <p>{cartitem.name}</p>
-        </div>
-        <div className='cartQty'>
-          <p>{cartitem.qty} liters</p>
-        </div>
-        <div className='cartRemove'>
-          <button id={cartitem._id} type="button" onClick={deleteItem}>Remove</button>
+      <div className="cartContents">
+        <div className="cartData">
+          <div className="cartProduct">
+            <p>{cartitem.name}</p>
+          </div>
+          <div className="cartQty">
+            <p>{cartitem.qty}L</p>
+          </div>
+          <div className="cartRemove">
+            <button id={cartitem._id} type="button" onClick={deleteItem}>
+              <span id="removeText">Remove</span>
+              <span id="xText">X</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Cartdetails
+export default Cartdetails;
